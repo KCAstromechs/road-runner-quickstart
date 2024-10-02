bdbc0957fd7aebd761d04dea376e1f6684611700
Dojiawesome2000 <145794573+Dojiawesome2000@users.noreply.github.com>
2024-10-01 19:13:28 -0500

    gain values updated (in mecanum drvie)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -82,13 +82,13 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 8.0;
-        public double lateralGain = 8.0;
-        public double headingGain = 12.0; // shared with turn
+        public double axialGain = 10.0;
+        public double lateralGain = 16.0;
+        public double headingGain = 115.0;
 
-        public double axialVelGain = 0.0;
-        public double lateralVelGain = 0.0;
-        public double headingVelGain = 2.0; // shared with turn
+        public double axialVelGain = 0.5;
+        public double lateralVelGain = 0.5;
+        public double headingVelGain = 0.5;
     }
 
     public static Params PARAMS = new Params();
